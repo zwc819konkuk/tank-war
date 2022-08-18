d9e9082e3e8fd117d9cd8851becb2c0e9e29b0e1
zwc819konkuk <104195503+zwc819konkuk@users.noreply.github.com>
2022-08-19 00:23:26 +0900

    定义子弹类，设定属性和方法，new出一颗新的子弹在画布上画出来

---
+++ src/com/zwc/tank/Bullet.java
@@ -0,0 +1,44 @@
+package com.zwc.tank;
+
+import java.awt.*;
+
+public class Bullet {
+    private static final int SPEED = 5;
+    private static final int WIDTH = 10, HEIGHT = 10 ;
+
+    private int x,y;
+    private Dir dir;
+
+    public Bullet(int x, int y, Dir dir) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+    }
+
+    public Bullet() {
+    }
+
+    public void paint(Graphics g) {
+        Color color = g.getColor();
+        g.setColor(Color.RED);
+        g.fillOval(x, y, WIDTH, HEIGHT); //正方形的左上角坐标和宽高
+        g.setColor(color);
+        move();
+    }
+    private void move() {
+        switch (dir) {
+            case LEFT:
+                x -= SPEED;
+                break;
+            case UP:
+                y -= SPEED;
+                break;
+            case RIGHT:
+                x += SPEED;
+                break;
+            case DOWN:
+                y += SPEED;
+                break;
+        }
+    }
+}
--- src/com/zwc/tank/TankFrame.java
+++ src/com/zwc/tank/TankFrame.java
@@ -5,7 +5,7 @@ import java.awt.event.*;
 
 public class TankFrame extends Frame {
     Tank myTank = new Tank(200, 200, Dir.DOWN);
-
+    Bullet bullet = new Bullet(300,300,Dir.DOWN);
 
     public TankFrame() {
         setSize(800, 600);
@@ -31,6 +31,7 @@ public class TankFrame extends Frame {
     @Override
     public void paint(Graphics g) {
         myTank.paint(g);
+        bullet.paint(g);
     }
 
 
