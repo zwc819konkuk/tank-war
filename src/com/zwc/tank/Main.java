4133489b7a77af3fb4803be8504491e1b59597f8
zwc819konkuk <104195503+zwc819konkuk@users.noreply.github.com>
2022-08-18 21:36:03 +0900

    改名

--- src/com/zwc/tank/Main.java
+++ src/com/zwc/tank/Main.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class T {
+public class Main {
     public static void main(String[] args) {
         TankFrame tf = new TankFrame();
 
